2f5cfe58b031db4c67ba195de8126d8764f9bead
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-11-17 22:00:41 +0600

    update project

--- client/src/pages/Home/Banner/Banner.jsx
+++ client/src/pages/Home/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <section>
             <div
-                className="hero inline-block min-h-screen lg:pt-96 lg:pb-40 bg-fixed"
+                className="hero inline-block lg:pt-96 lg:pb-40 bg-fixed"
                 style={{
                     backgroundImage: `url(${zahidulIslamImg})`,
                     width: '100%',
@@ -16,8 +16,8 @@ const Banner = () => {
                 <div className="hero-overlay bg-opacity-0"></div>
                 <div className="hero-content block text-neutral-content text-right">
                     <div className="text-neutral mt-20 lg:mt-96">
-                        <h1 className="mb-5 text-4xl font-medium font-family mt-96 text-right">Zahidul Islam Jobair</h1>
-                        <p className="mb-5 text-md font-normal">
+                        <h1 className="mb-5 text-2xl lg:text-4xl font-medium font-family mt-96 text-right">Zahidul Islam Jobair</h1>
+                        <p className="mb-5 text-base lg:text-md font-normal">
                             Lecturer, Department of Journalism, Media and Communication <br />
                             Daffodil International University
                         </p>
